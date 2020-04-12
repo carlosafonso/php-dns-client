2c2b7ca75e5b7af13b63004fdec578b26f71fa49
Carlos Afonso Pérez <carlos.afonso.perez@gmail.com>
2020-04-12 18:07:06 +0200

    Add provider to RequestTest to test all valid types that are supported

--- tests/RequestTest.php
+++ tests/RequestTest.php
@@ -8,7 +8,10 @@ use InvalidArgumentException;
 
 class RequestTest extends \PHPUnit\Framework\TestCase
 {
-    public function testCreatingAValidRequest()
+    /**
+     * @dataProvider validTypesProvider
+     */
+    public function testCreatingAValidRequest($type)
     {
         $request = new Request('foo.com', 0x0001);
 
@@ -21,5 +24,21 @@ class RequestTest extends \PHPUnit\Framework\TestCase
         $this->expectException(InvalidArgumentException::class);
 
         new Request('foo.com', 0xFFFF);
+    }
+
+    public function validTypesProvider()
+    {
+        return [
+            [0x0001],
+            [0x0002],
+            [0x0005],
+            [0x0006],
+            [0x000B],
+            [0x000C],
+            [0x000F],
+            [0x0021],
+            [0x001C],
+            [0x00FF]
+        ];
     }
 }
